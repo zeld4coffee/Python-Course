33500fe8046031de1fcb003dfe5f03e2a8635a01
AiltuCesar <71790487+zeld4coffee@users.noreply.github.com>
2021-08-11 14:28:04 -0300

    Add files via upload

---
+++ mary/main.py
@@ -0,0 +1,8 @@
+
+print("Só quero dizer uma coisa...")
+print("EI PRESTA ATENÇÃO!")
+print("nenhum código python é capaz de suportar a frequência de bits")
+print("que meu coração suporta as batidas quando te vê")
+print('Eu te amo muito, Nunca esqueça disso...')
+print("minha princesa <")
+
