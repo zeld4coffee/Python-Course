c8411f5a5dc17a46eb626adf2608d7c3b94bf781
AiltuCesar <71790487+zeld4coffee@users.noreply.github.com>
2021-08-11 13:37:59 -0300

    Add files via upload

---
+++ Primeiro Programa em Python/main.py
@@ -0,0 +1,21 @@
+# valores que são armazenados nas variávei A e B
+va = 3
+vb = 2
+
+# soma
+soma = va + vb
+# multiplicação
+multi = va * vb
+# divisão
+div = va / vb
+# subtração
+sub = va - vb
+# módulo (resto de uma divisão)
+mod = va % vb
+
+# saída de dados
+print("A Soma de", va, "e", vb, "resulta em", soma)
+print("A Multiplicação de", va, "e", vb, "resulta em", multi)
+print("A Divisão de", va, "e", vb, "resulta em", div)
+print("A Subtração de", va, "e", vb, "resulta em", multi)
+print("A Divisão de", va, "e", vb, "tem como resto", mod)
