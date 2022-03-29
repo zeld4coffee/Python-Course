1ce477152f6d125064ac6df9db82e27d97d43c90
AiltuCesar <ailtonc.n64@gmail.com>
2022-03-29 08:13:33 -0300

    Update main.py

--- mary/main.py
+++ mary/main.py
@@ -4,5 +4,5 @@ print("EI PRESTA ATENÇÃO!")
 print("nenhum código python é capaz de suportar a frequência de bits")
 print("que meu coração suporta as batidas quando te vê")
 print('Eu te amo muito, Nunca esqueça disso...')
-print("minha princesa <")
+print("minha princesa <3")
 
